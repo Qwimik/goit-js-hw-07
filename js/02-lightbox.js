dbb8941385fef1e6f184dcb12af67868c0aa1c50
Oleksii Rozhnenko <qwimik@gmail.com>
2023-02-06 21:57:20 +0200

    complete lightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+galleryEl.innerHTML = galleryItems
+  .map(item => {
+    return `<a class="gallery__item" href="${item.original}">
+    <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+  </a>`;
+  })
+  .join('');
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+galleryEl.addEventListener('click', onImgClick);
+
+function onImgClick(e) {
+  e.preventDefault();
+
+  if (e.target.classList.value !== 'gallery__image') {
+    return;
+  }
+  lightbox.on('show.simplelightbox', () => {});
+}
