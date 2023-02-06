82299afb36989d057370d1ed16a69889a379ed9f
Oleksii Rozhnenko <qwimik@gmail.com>
2023-02-06 22:05:36 +0200

    update gallery and lightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -4,9 +4,9 @@ import { galleryItems } from './gallery-items.js';
 const galleryEl = document.querySelector('.gallery');
 
 galleryEl.innerHTML = galleryItems
-  .map(item => {
-    return `<a class="gallery__item" href="${item.original}">
-    <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+  .map(({ preview, original, description }) => {
+    return `<a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
   </a>`;
   })
   .join('');
